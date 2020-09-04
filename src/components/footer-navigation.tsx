98ca3bb243d4906616b8d9953edd97859e95b1c8
Matthias Reis <matthias.reis@xing.com>
2020-09-04 08:57:29 +0200

    footer styling and responsiveness

--- src/components/footer-navigation.tsx
+++ src/components/footer-navigation.tsx
@@ -1,25 +1,51 @@
-import * as React from 'react';
-import styled from '@emotion/styled';
-import { Container } from './container';
-import { color } from '../style';
+import * as React from "react";
+import styled from "@emotion/styled";
+import { Container } from "./container";
+import { color, space, font } from "../style";
 
 const Section = styled.footer`
   background: ${color.neutral[2]};
 `;
 
+const Copyright = styled.p`
+  color: ${color.neutral[3]};
+  text-align: center;
+`;
+
+const Navigation = styled.nav`
+  text-align: center;
+  margin-bottom: ${space[3]};
+`;
+
+const Item = styled.a`
+  display: inline-block;
+  padding: 0 ${space[1]};
+  text-decoration: none;
+  color: ${color.cold[2]};
+  font-family: ${font.title};
+
+  &:hover,
+  &:focus {
+    color: ${color.neutral[5]};
+    background: ${color.cold[0]};
+    border-radius: ${space[1]};
+  }
+`;
+
 export const FooterNavigation: React.FC = () => (
   <Section>
     <Container large>
-      <p>© {new Date().getFullYear()} Anne bloggt cardamonchai.</p>
-      <ul>
-        <li>
-          <a href="/archive">Archiv</a>
-          <a href="/">für Affiliates</a>
-          <a href="/">Impressum</a>
-          <a href="/">Datenschutz</a>
-          <a href="/">Transparenz</a>
-        </li>
-      </ul>
+      <Copyright>
+        © {new Date().getFullYear()} · Cardamonchai.com · Rock'n'Roll Vegan.
+      </Copyright>
+      <Navigation>
+        <Item href="/archive">Archiv</Item>
+        <Item href="/labels">Themen</Item>
+        <Item href="/">für Affiliates</Item>
+        <Item href="/">Impressum</Item>
+        <Item href="/">Datenschutz</Item>
+        <Item href="/">Transparenz</Item>
+      </Navigation>
     </Container>
   </Section>
 );
