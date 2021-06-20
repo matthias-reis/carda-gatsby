6401b4fa98601e0d0547f82627c4abb8c748bbce
github-script <github-script@gnix.com>
2021-06-20 12:30:29 +0200

    feat: newsletter icon linked to landing page

--- src/components/interaction-newsletter.tsx
+++ src/components/interaction-newsletter.tsx
@@ -3,7 +3,8 @@ import styled from '@emotion/styled';
 import { color, fontSize, space } from '../style';
 import { IconNewsletter } from './icons';
 
-const Box = styled.div`
+const Box = styled.a`
+  display: block;
   margin-bottom: ${space[2]};
   color: ${color.text20};
 
@@ -18,7 +19,7 @@ const Label = styled.div`
 `;
 
 export const InteractionNewsletter: React.FC = () => (
-  <Box>
+  <Box href="https://mailchi.mp/94bdbb6fded3/cardamonchai">
     <IconNewsletter />
     <Label>Newsletter</Label>
   </Box>
