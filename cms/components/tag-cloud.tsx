e156188721847ffef7e7907187e9a24d08f82b3e
matthias.reis <matthias.reis@xing.com>
2024-01-26 20:08:54 +0100

    feat(anne-cms): add meta data editor tab

--- cms/components/tag-cloud.tsx
+++ cms/components/tag-cloud.tsx
@@ -1,5 +1,42 @@
 import { FC } from 'react';
+import { Cross2Icon as CloseIcon } from '@radix-ui/react-icons';
+import { Input } from './ui/input';
 
-export const TagCloud: FC<{ className: string }> = ({ className }) => {
-  return <div className={className}>Tag Cloud</div>;
+export const TagCloud: FC<{
+  className: string;
+  value?: string[];
+  onChange: (tags: string[]) => void;
+}> = ({ className, value = [], onChange }) => {
+  return (
+    <ul className={`flex gap-2 flex-wrap ${className}`}>
+      {value.map((t) => (
+        <li
+          key={t}
+          className="rounded-full bg-stone-800 font-condensed uppercase font-bold text-xs flex gap-1 items-center pl-3"
+        >
+          <span>{t}</span>
+          <button
+            className="rounded-full w-8 h-8 bg-stone-700 hover:bg-stone-900 grid place-content-center m-1"
+            onClick={() => {
+              const newList = value.filter((x) => x !== t);
+              onChange(newList);
+            }}
+          >
+            <CloseIcon />
+          </button>
+        </li>
+      ))}
+      <Input
+        className="w-40"
+        placeholder="noch eins"
+        onKeyDown={(ev) => {
+          if (ev.key === 'Enter') {
+            const newList = [...value, ev.currentTarget.value];
+            onChange(newList);
+            ev.currentTarget.value = '';
+          }
+        }}
+      />
+    </ul>
+  );
 };
--- cms/core/editor-content.tsx
+++ cms/core/editor-content.tsx
@@ -8,9 +8,12 @@ import { DatePicker } from '@/components/datepicker';
 import { format } from 'date-fns';
 import { Textarea } from '@/components/ui/textarea';
 import { TagCloud } from '@/components/tag-cloud';
-import { ToggleGroup, ToggleGroupItem } from '@radix-ui/react-toggle-group';
+import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
 
-export const EditorContent: FC<{ article: Article }> = ({ article }) => {
+export const EditorContent: FC<{
+  article: Article;
+  change: (changeFn: (a: Article) => Article) => void;
+}> = ({ article, change }) => {
   return (
     <div className="p-3 font-condensed">
       <Tabs defaultValue="meta">
@@ -26,54 +29,192 @@ export const EditorContent: FC<{ article: Article }> = ({ article }) => {
           <div className="max-w-xl mx-auto flex flex-col gap-1">
             <Card className="p-3 flex flex-col gap-1 mb-6">
               <Field label="Slug">
-                <Input id="Slug" />
+                <Input
+                  id="Slug"
+                  value={article.slug}
+                  onChange={(ev) => {
+                    const slug = ev.target.value;
+                    change((a: Article) => ({ ...a, slug }));
+                  }}
+                />
               </Field>
 
               <Field label="Datum">
-                <DatePicker id="Datum" />
+                <DatePicker
+                  id="Datum"
+                  value={new Date(article.date)}
+                  onChange={(date: Date | undefined) => {
+                    if (!date) return;
+                    change((a: Article) => ({
+                      ...a,
+                      date: date.toISOString(),
+                    }));
+                  }}
+                />
               </Field>
               <div className="flex text-sm text-stone-400 justify-end gap-4">
                 <div className="opacity-40">Neuer Pfad:</div>
                 <pre className="text-sm text-stone-400">
-                  /{format(new Date(), 'yyyy-MM')}/{article.slug}/
+                  /{format(new Date(article.date), 'yyyy-MM')}/{article.slug}/
                 </pre>
               </div>
             </Card>
             <Field label="Titel">
-              <Input id="Titel" />
+              <Input
+                id="Titel"
+                value={article.title}
+                onChange={(ev) => {
+                  const title = ev.target.value;
+                  change((a: Article) => ({ ...a, title }));
+                }}
+              />
+            </Field>
+            <Field label="Typ">
+              <Textarea
+                rows={2}
+                id="Typ"
+                value={article.typeName}
+                onChange={(ev) => {
+                  const typeName = ev.target.value;
+                  change((a: Article) => ({ ...a, typeName }));
+                }}
+              />
             </Field>
             <Field label="Untertitel">
-              <Input id="Untertitel" />
+              <Textarea
+                rows={2}
+                id="Untertitel"
+                value={article.subTitle}
+                onChange={(ev) => {
+                  const subTitle = ev.target.value;
+                  change((a: Article) => ({ ...a, subTitle }));
+                }}
+              />
             </Field>
             <Field label="SEO-Titel">
-              <Input id="SEO-Titel" />
+              <Textarea
+                rows={2}
+                id="SEO-Titel"
+                value={article.seoTitle}
+                onChange={(ev) => {
+                  const seoTitle = ev.target.value;
+                  change((a: Article) => ({ ...a, seoTitle }));
+                }}
+              />
             </Field>
             <Field label="Social Titel">
-              <Input id="Social Titel" className="mb-6" />
+              <Textarea
+                rows={2}
+                id="Social Titel"
+                value={article.ogTitle}
+                onChange={(ev) => {
+                  const ogTitle = ev.target.value;
+                  change((a: Article) => ({ ...a, ogTitle }));
+                }}
+                className="mb-6"
+              />
             </Field>
             <Field label="Description">
-              <Textarea id="Description" />
+              <Textarea
+                id="Description"
+                rows={6}
+                value={article.description}
+                onChange={(ev) => {
+                  const description = ev.target.value;
+                  change((a: Article) => ({ ...a, description }));
+                }}
+              />
             </Field>
             <Field label="Excerpt">
-              <Textarea id="Excerpt" className="mb-6" />
+              <Textarea
+                id="Excerpt"
+                rows={6}
+                className="mb-6"
+                value={article.excerpt}
+                onChange={(ev) => {
+                  const excerpt = ev.target.value;
+                  change((a: Article) => ({ ...a, excerpt }));
+                }}
+              />
             </Field>
             <Field label="Bild">
-              <Input id="Bild" />
+              <div className="flex gap-2">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  src={article.ogImage}
+                  alt="Social Bild"
+                  className="h-14 object-cover rounded"
+                />
+                <Input
+                  id="Bild"
+                  className="text-xs"
+                  value={article.image}
+                  onChange={(ev) => {
+                    const image = ev.target.value;
+                    change((a: Article) => ({ ...a, image }));
+                  }}
+                />
+              </div>
             </Field>
             <Field label="Bild Copyright">
-              <Input id="Bild Copyright" />
+              <Input
+                id="Bild Copyright"
+                value={article.imageCopyright}
+                onChange={(ev) => {
+                  const imageCopyright = ev.target.value;
+                  change((a: Article) => ({ ...a, imageCopyright }));
+                }}
+              />
             </Field>
             <Field label="Social Bild">
-              <Input id="Social Bild" className="mb-6" />
+              <div className="flex gap-2 mb-6">
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img
+                  src={article.ogImage}
+                  alt="Social Bild"
+                  className="h-14 object-cover rounded"
+                />
+                <Input
+                  id="Social Bild"
+                  className="text-xs "
+                  value={article.ogImage}
+                  onChange={(ev) => {
+                    const ogImage = ev.target.value;
+                    change((a: Article) => ({ ...a, ogImage }));
+                  }}
+                />
+              </div>
             </Field>
-            <Field label="Tags">
-              <TagCloud className="mb-6" />
+            <Field label="Stichwörter">
+              <TagCloud
+                className="mb-6"
+                value={article.labels}
+                onChange={(labels) => {
+                  change((a: Article) => ({ ...a, labels }));
+                }}
+              />
             </Field>
             <Field label="Werbung">
-              <ToggleGroup type="multiple">
+              <ToggleGroup
+                type="multiple"
+                variant="outline"
+                onValueChange={(value) => {
+                  change((a: Article) => ({
+                    ...a,
+                    advertisement: value.includes('ad'),
+                    affiliate: value.includes('af'),
+                    productsProvided: value.includes('pr'),
+                  }));
+                }}
+                value={[
+                  article.advertisement ? 'ad' : '',
+                  article.affiliate ? 'af' : '',
+                  article.productsProvided ? 'pr' : '',
+                ].filter(Boolean)}
+              >
                 <ToggleGroupItem value="ad">Werbung</ToggleGroupItem>
                 <ToggleGroupItem value="af">Affiliate</ToggleGroupItem>
-                <ToggleGroupItem value="pr">Produktplatzierung</ToggleGroupItem>
+                <ToggleGroupItem value="pr">Produkte bekommen</ToggleGroupItem>
               </ToggleGroup>
             </Field>
           </div>
